351da65ff88d0b17e759c19a00e47b262e7eec2e
yuhao <yuhao_4220@qq.com>
2026-04-09 17:19:32 +0800

    update

--- h20e-1/GLM-5-AWQ-1/start.sh
+++ h20e-1/GLM-5-AWQ-1/start.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-apt update && apt install -y git
+#apt update && apt install -y git
 # pip install mooncake-transfer-engine
-pip install transformers -U
-pip install --upgrade transformers
+#pip install transformers -U
+#pip install --upgrade transformers
 #pip install git+https://github.com/deepseek-ai/DeepGEMM.git@v2.1.1.post3 --no-build-isolation
 python3 /data/glm4_mtp_fix.py
-
+#pip install -i https://test.pypi.org/simple/ lmcache==0.4.4.dev5
 export PYTHORCH_ALLOC_CONF=expandable_segments:True,max_non_split_rounding_mb:256
 export VLLM_USE_V1=1
-
+#export LMCACHE_CONFIG_FILE=/data/lmcache_config.yaml
+#export PYTHONHASHSEED=0
 echo $CUDA_VISIBLE_DEVICES
-ls /dev/nvidia*
-# mooncake_http_metadata_server &
-# mooncake_master &
 
 vllm serve /data/models/tclf90/GLM-5-AWQ \
     --served-model-name 'zai-org/GLM-5' \
-    --max-num-seqs 8 \
+    --max-num-seqs 64 \
     --max-model-len 202752 \
     --enable-chunked-prefill \
-    --max-num-batched-tokens 4096 \
-    --gpu-memory-utilization 0.92 \
+    --max-num-batched-tokens 16384 \
+    --gpu-memory-utilization 0.93 \
     --enable-prefix-caching \
     --enable-expert-parallel \
     --tensor-parallel-size 4 \
@@ -31,6 +29,7 @@ vllm serve /data/models/tclf90/GLM-5-AWQ \
     --trust-remote-code \
     --host 0.0.0.0 \
     --port 8000 \
-    --async-scheduling \
-    --speculative-config '{"method":"mtp", "num_speculative_tokens":1}' 
+    --speculative-config '{"method":"mtp", "num_speculative_tokens":1}'  
+#    --kv-transfer-config \
+#    '{"kv_connector":"LMCacheConnectorV1", "kv_role":"kv_both" }'
 
